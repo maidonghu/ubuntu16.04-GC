7c3b7a84e0ad146528a4d08ba3068d4eade9483a
maidonghu <maidonghu@users.noreply.github.com>
2017-03-08 22:56:59 -0800

    Update ghost_nginx.sh

--- ghost_nginx.sh
+++ ghost_nginx.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs unzip
+apt-get install -y nodejs unzip
 
-sudo mkdir -p /var/www/
+mkdir -p /var/www/
 cd /var/www/
-sudo wget https://ghost.org/zip/ghost-latest.zip
-sudo unzip -d ghost ghost-latest.zip
+wget https://ghost.org/zip/ghost-latest.zip
+unzip -d ghost ghost-latest.zip
 
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-GC/master/ghost_config.js
-sudo mv ghost_config.js config.js
+mv ghost_config.js config.js
 
 nginx=stable
-echo -e "\r" | sudo add-apt-repository ppa:nginx/$nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+echo -e "\r" | add-apt-repository ppa:nginx/$nginx
+apt-get update
+apt-get install -y nginx
 cd /etc/nginx/
-sudo rm sites-enabled/default
+rm sites-enabled/default
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-GC/master/ghost
-sudo mv ghost  /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost
+mv ghost  /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost
 wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-GC/master/nginx.conf
-sudo mv nginx.conf /etc/nginx/
+mv nginx.conf /etc/nginx/
 
-sudo useradd ghost
-sudo mkdir -p /home/ghost
-sudo chown -R ghost:ghost /home/ghost
-sudo chown -R ghost:ghost /var/www/ghost
+useradd ghost
+mkdir -p /home/ghost
+chown -R ghost:ghost /home/ghost
+chown -R ghost:ghost /var/www/ghost
 
+su -c "cd /var/www/ghost; npm install --production" ghost
 su -c "cd /var/www/ghost; npm install forever" ghost
 su -c "cd /var/www/ghost; NODE_ENV=production /var/www/ghost/node_modules/forever/bin/forever start index.js" ghost
 
-sudo service nginx restart
+service nginx restart
